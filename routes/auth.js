3e0587db9103529b41e9ce58e128ecb78a9c17e9
VikashMeena777 <136349370+VikashMeena777@users.noreply.github.com>
2025-08-29 18:26:36 +0530

    Add auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {
-  register,
-  login,
-  getUser,
-  editProfile,
-  changePassword,
-  authMiddleware
-} = require("../controllers/authController");
+const authController = require("../controllers/authController");
 
-router.post("/register", register);
-router.post("/login", login);
-router.get("/user", authMiddleware, getUser);
-router.post("/edit-profile", authMiddleware, editProfile);
-router.post("/change-password", authMiddleware, changePassword);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/user", authController.authMiddleware, authController.getUser);
+router.put("/edit", authController.authMiddleware, authController.editProfile);
+router.put("/change-password", authController.authMiddleware, authController.changePassword);
 
 module.exports = router;
