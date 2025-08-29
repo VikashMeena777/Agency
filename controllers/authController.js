82cd84a51dd9238615642c888a2386639e370ca7
VikashMeena777 <136349370+VikashMeena777@users.noreply.github.com>
2025-08-29 18:26:31 +0530

    Add authController to manage user authentication and authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ exports.register = async (req, res) => {
       return res.status(400).json({ message: "Password too short" });
 
     const passwordHash = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, email, passwordHash });
+    await User.create({ name, email, passwordHash });
     return res.status(201).json({ message: "User registered" });
   } catch (err) {
     return res.status(400).json({ message: err.message });
